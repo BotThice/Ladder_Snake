e7bf35f6b4fcc9fd86bd95852ce60f7f3a4f6ab3
BotThice <thiranut.120346@gmail.com>
2024-04-21 14:42:13 +0700

    more Player testcase

--- PlayerTest.java
+++ PlayerTest.java
@@ -6,6 +6,7 @@ class PlayerTest {
 
     @Test
     void moveMethod(){
+
         Player p1 = new Player(1,"1");
         //Normal Case
         p1.move(50,100);
@@ -15,6 +16,12 @@ class PlayerTest {
         p1.setPosition(1);
         p1.move(10,10);
         assertEquals(9,p1.getPosition());
+
+        //With dice
+        p1.setPosition(1);
+        Game g = new Game();
+        p1.move(g.rollDice(),100);
+        assertTrue(p1.getPosition()<=7 && p1.getPosition()>=2);
     }
 
 }

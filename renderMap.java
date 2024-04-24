59bb83e612e57d2ab6cac24e726317f6432a6b86
BotThice <thiranut.120346@gmail.com>
2024-04-24 16:06:29 +0700

    Deleted not related file

--- renderMap.java
+++
@@ -1,8 +0,0 @@
-
-public class renderMap {
-    public static void main(String[] args) {
-        Board b = new Board(10,10);
-        b.renderMap();
-        System.out.println(String.format("%"));
-    }
-}
